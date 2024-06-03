b26ef237b90b24b9f080c1c0ce6b26e5432d0ca1
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-03 19:29:33 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -7,18 +7,3 @@ resource "aws_s3_bucket" "george" {
   }
 }
 
-resource "aws_s3_bucket" "george" {
-  bucket = "example-bucket"
-}
-
-resource "aws_s3_bucket_acl" "george" {
-  bucket = aws_s3_bucket.george.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_versioning" "versioning_george" {
-  bucket = aws_s3_bucket.george.id
-  versioning_configuration {
-    status = "Disabled"
-  }
-}
