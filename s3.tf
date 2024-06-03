f680498cc6b55cf8a0aec5c669081b2dc307e9cc
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-03 19:22:23 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -4,19 +4,3 @@ resource "aws_s3_bucket" "bucket1" {
         name="test-bucket-${count.index}"
         }
 }
-resource "aws_s3_bucket" "bucket1" {
-  bucket = "bucket1-bucket"
-}
-
-resource "aws_s3_bucket_acl" "bucket1" {
-  bucket = aws_s3_bucket.bucket1.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_versioning" "versioning_bucket1" {
-  bucket = aws_s3_bucket.bucket1.id
-  versioning_configuration {
-    status = "Disabled"
-  }
-}
-
