65803db56d45da75fbe93f11c49a35da8cba17e0
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-03 20:02:32 -0400

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,25 +1,46 @@
-resource "aws_security_group" "allow_http" {
-  name        = "allow_http"
-  description = "Allow http"
-  }
-
-ingress {
-    protocol = "tcp"
-    cidr_blocks=["0.0.0.0/0"]
-    self      = true
-    from_port = 80
-    to_port   = 80
-  }
-
-ingress {
-  cidr_ipv6         = ["0.0.0.0/0"]
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
+#Create Security Group -SSH Traffic
+resource "aws_security_group" "vpc-ssh" {
+    name="vpc-ssh"
+    description = "Dev VPC SSH"
+    ingress{
+        description="Allow port 22"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks=["0.0.0.0/0"]
+    }
+    egress{
+        description = "Allow all ip and ports outbound"
+        from_port=0
+        to_port=0
+        protocol="-1"
+        cidr_blocks=["0.0.0.0/0"]
+    }
+    tags = {
+      Name="vpc-ssh"
+    }
 }
 
-egress{
-        description = "Allow all outbound"
+#Create Security Group - HTTP Traffic
+resource "aws_security_group" "vpc-web" {
+    name="vpc-web"
+    description = "Dev VPC web"
+    ingress{
+        description="Allow port 80"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks=["0.0.0.0/0"]
+    }
+    ingress{
+        description="Allow port 443"
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks=["0.0.0.0/0"]
+    }
+    egress{
+        description = "Allow all ip and ports outbound"
         from_port=0
         to_port=0
         protocol="-1"
