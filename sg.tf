1b83ea4f8237a197f551cfd225c03e8c6ece51a8
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-03 20:00:44 -0400

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,21 +1,32 @@
-resource "aws_vpc" "mainvpc" {
-  cidr_block = "10.1.0.0/16"
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow http"
+  }
 }
 
-resource "aws_default_security_group" "default" {
-  vpc_id = aws_vpc.mainvpc.id
-
-  ingress {
-    protocol  = -1
+ingress {
+    protocol = "tcp"
+    cidr_blocks=["0.0.0.0/0"]
     self      = true
-    from_port = 0
-    to_port   = 0
+    from_port = 80
+    to_port   = 80
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+ingress {
+  cidr_ipv6         = ["0.0.0.0/0"]
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+egress{
+        description = "Allow all outbound"
+        from_port=0
+        to_port=0
+        protocol="-1"
+        cidr_blocks=["0.0.0.0/0"]
+    }
+    tags = {
+      Name="vpc-web"
+    }
 }
