5e2c8b9b2ea9d37222d90f658719d0f9b90dc4c0
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-05 19:45:50 -0400

    Update module.tf

--- module.tf
+++ module.tf
@@ -17,16 +17,5 @@ module "ec2_instance" {
 module "ec2_instance_george" {
   source   = "./aws/"
 
-  name = "single-instance"
-
-  instance_type          = "t2.micro"
-  key_name               = "user1"
-  monitoring             = true
-  vpc_security_group_ids = ["sg-12345678"]
-  subnet_id              = "subnet-eddcdzz4"
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
   }
 }
