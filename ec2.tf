17a9db5f1158acbcd13b517eab160edca84c5ffb
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-05 19:01:24 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,32 @@
-resource "aws_instance""myec2m"{
-    ami=data.aws_ami.amz_linux2.id
-    instance_type = var.instance_type
-    vpc_security_group_ids = [aws_security_group.vpc-ssh.id,aws_security_group.vpc-web.id]
-    count = 2
-    tags = {
-        name="count-demo-${count.index}"
-    }
+locals{
+  linux_app=[for f in fileset("${path.module}/$aws", "[^_]*.yaml") : yamldecode(file("${path.module}/$aws/${f}"))]
+  linux_app_list = flatten([
+    for app in local.linux_app : [
+      for linuxapps in try(app.listoflinuxapp, []) :{
+        ami=data.aws_ami.amz_linux2.id
+        instance_type = var.instance_type
+        sku_name=linuxapps.sku_name  
+        vpc_security_group_ids = [aws_security_group.vpc-ssh.id,aws_security_group.vpc-web.id]
+      }
+    ]
+])
 }
+resource "aws_instance""myec2m"{ {
+  for_each            ={for sp in local.linux_app_list: "${sp.name}"=>sp }
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.georgeibrahim.name
+  location            = azurerm_resource_group.georgeibrahim.location
+  os_type             = each.value.os_type
+  sku_name            = each.value.sku_name
+}
+
+resource "aws_instance""myec2m"{ {
+  for_each            = azurerm_service_plan.george
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.georgeibrahim.name
+  location            = azurerm_resource_group.georgeibrahim.location
+  service_plan_id     = each.value.id
+ site_config {}
+}
+
+
