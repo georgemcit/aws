48c3eaa7105b4c75b9b65596f736fdea48f2fbb2
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-05 20:23:06 -0400

    Update ec21.tf

--- ec21.tf
+++ ec21.tf
@@ -8,35 +8,11 @@ locals{
     ]
 ])
 }
-resource "aws_vpc" "example" {
-  cidr_block = "172.16.0.0/16"
 
-  tags = {
-    Name = "tf-example"
-  }
-}
 
-resource "aws_subnet" "example" {
-  vpc_id            = aws_vpc.example.id
-  cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-east-2a"
-
-  tags = {
-    Name = "tf-example"
-  }
-}
-
-data "aws_ami" "amzn-linux-2023-ami" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["al2023-ami-2023.*-x86_64"]
-  }
-}
-
-resource "aws_instance" "example" {
+resource "aws_instance" "george" {
+  for_each            ={for sp in ec2instance_list: "${sp.name}"=>sp }
+  name                = each.value.name
   ami           = data.aws_ami.amzn-linux-2023-ami.id
   instance_type = "c6a.2xlarge"
   subnet_id     = aws_subnet.example.id
@@ -47,6 +23,6 @@ resource "aws_instance" "example" {
   }
 
   tags = {
-    Name = "tf-example"
+    Name = "tf-george"
   }
 }
