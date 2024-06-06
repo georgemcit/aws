e282c6cb12861c134a675763f1cc7513b36b581f
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-05 20:10:23 -0400

    Update ec21.tf

--- ec21.tf
+++ ec21.tf
@@ -1,11 +1,52 @@
+locals{
+  ec2_instance=[for f in fileset("${path.module}/aws", "[^_]*.yaml") : yamldecode(file("${path.module}/aws/${f}"))]
+  ec2instance_list = flatten([
+    for app in local.ec2_instance: [
+      for ec2 in try(app.listofec2, []) :{
+        ami=data.aws_ami.amz_linux2.id
+      }
+    ]
+])
+}
+resource "aws_vpc" "example" {
+  cidr_block = "172.16.0.0/16"
 
-resource "aws_instance""myec2m"{
-    ami=data.aws_ami.amz_linux2.id
-    instance_type = var.instance_type
-    vpc_security_group_ids = [aws_security_group.vpc-ssh.id,aws_security_group.vpc-web.id]
-    count = 2
-    tags = {
-        name="count-demo-${count.index}"
-    }
+  tags = {
+    Name = "tf-example"
+  }
 }
 
+resource "aws_subnet" "example" {
+  vpc_id            = aws_vpc.example.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+data "aws_ami" "amzn-linux-2023-ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.amzn-linux-2023-ami.id
+  instance_type = "c6a.2xlarge"
+  subnet_id     = aws_subnet.example.id
+
+  cpu_options {
+    core_count       = 2
+    threads_per_core = 2
+  }
+
+  tags = {
+    Name = "tf-example"
+  }
+}
