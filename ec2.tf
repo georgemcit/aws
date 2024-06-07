9f4c9c55e378ece3c3d8d4def092065e2e5c61c7
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-07 19:52:14 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,4 @@
+/*
 # Bastion Host - EC2 Instance that will be created in VPC Public Subnet
 module "ec2_public" {
   source  = "terraform-aws-modules/ec2-instance/aws"
@@ -15,3 +16,4 @@ module "ec2_public" {
   vpc_security_group_ids = [module.public_bastion_sg.security_group_id]
   tags = local.common_tags
 }
+*/
