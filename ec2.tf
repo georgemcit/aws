ff3303bbc1b55730567ed778c68bfa3df4af7472
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-06 23:52:04 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 locals{
-  awsec2 = yamldecode(file("${path.module}/aws/${f}"))]
+  awsec2 = yamldecode(file("${path.module}/aws/${f}"))
   awsec2_list = [
     for value in local.awsec2.listofec2: {
         name=value.tagname
