d8e9baa516c1f87ad1df6b4e228449eb8b8f9838
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-06 22:19:10 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -10,7 +10,7 @@ aws_ec2=yamldecode(file("${path.module}/$aws/ec2.yaml"))
 }
 resource "aws_instance""awsec2"{ {
   for_each            ={for sp in local.awsec2_list: "${sp.name}"=>sp }
-  instance_type = var.instance_type
+  ami=data.aws_ami.amz_linux2.id
   instance_type       = each.value.instance_type
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id,aws_security_group.vpc-web.id]
    tags = {
