c56e0d1aa65a55edb2a3e62bc79b271bedc27cc9
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-07 19:37:45 -0400

    Update vairable.tf

--- vairable.tf
+++ vairable.tf
@@ -26,11 +26,7 @@ variable "instance_keypair" {
   type = string
   default = "terraform-key"
 }
-variable "aws_region" {
-  description = "Region in which AWS Resources to be created"
-  type = string
-  default = "us-east-1"  
-}
+
 # Environment Variable
 variable "environment" {
   description = "Environment Variable used as a prefix"
