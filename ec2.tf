c877d2ff34cf4029d31b1da7c54cc14003247d13
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-06 23:15:10 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 locals{
-aws_ec2 = yamldecode(file("${path.module}/aws/ec2.yaml"))
+    aws_ec2 = yamldecode(file("${path.module}/aws/ec2.yaml"))
     awsec2_list = [
       for value in local.aws_ec2.listofec2: {
          name=value.tagename
