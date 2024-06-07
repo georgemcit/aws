ebfe40840373a8ccbfc56dcef756f3860813ffce
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-06 23:06:09 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,3 @@
-resource "aws_instance""myec2m"{
-    ami=data.aws_ami.amz_linux2.id
-    instance_type = var.instance_type
-    vpc_security_group_ids = [aws_security_group.vpc-ssh.id,aws_security_group.vpc-web.id]
-    count = 2
-    tags = {
-        name="count-demo-${count.index}"
-    }
-}
-
 locals{
 aws_ec2 = yamldecode(file("${path.module}/aws/ec2.yaml"))
     awsec2_list = [
