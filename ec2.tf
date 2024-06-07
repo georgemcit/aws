df3c742f24f21ffcd3ff13766604d9294ef78176
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-06 22:22:57 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 
 locals{
 aws_ec2=yamldecode(file("${path.module}/$aws/ec2.yaml"))
-    awsec2_list= [
+    awsec2_list = [
       for value in loacl.aws_ec2.listofec2:{
         name=value.tagename
         instance_type=value.instance_type
