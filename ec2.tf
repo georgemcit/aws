9f68cac85c72ea88a3d42447b30bd3af5e7b1cbf
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-06 22:55:12 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -11,7 +11,7 @@ resource "aws_instance""myec2m"{
 locals{
 aws_ec2 = yamldecode(file("${path.module}/aws/ec2.yaml"))
     awsec2_list = [
-      for value in loacl.aws_ec2.listofec2:{
+      for value in local.aws_ec2.listofec2:{
          name=value.tagename
          instance_type=value.instance_type
        }
