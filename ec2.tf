5a3d2de239d8a8396de0b6aac2bcc6eed2ab1fe7
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-06 22:20:52 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,14 @@
 
 locals{
 aws_ec2=yamldecode(file("${path.module}/$aws/ec2.yaml"))
-    awsec2_list: [
+    awsec2_list= [
       for value in loacl.aws_ec2.listofec2{
         name=value.tagename
         instance_type=value.instance_type
     ]
 ])
 }
-resource "aws_instance""awsec2"{ {
+resource "aws_instance""awsec2"{ 
   for_each            ={for sp in local.awsec2_list: "${sp.name}"=>sp }
   ami=data.aws_ami.amz_linux2.id
   instance_type       = each.value.instance_type
