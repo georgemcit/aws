54758253073156494f4a23dc655149e6e40ce4f4
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-06 22:45:03 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -17,12 +17,13 @@ aws_ec2=yamldecode(file("${path.module}/$aws/ec2.yaml"))
        }
     ]
 }
-resource "aws_instance""myec2m"{
-  for_each = {for sp in local.awsec2_list: "${sp.name}"=>sp }
-  ami=data.aws_ami.amz_linux2.id
-  instance_type       = each.value.instance_type
-  vpc_security_group_ids = [aws_security_group.vpc-ssh.id,aws_security_group.vpc-web.id]
-   tags = {
-        name=each.value.name
+resource "aws_instance""awsec2_george"{
+    for_each = {for sp in local.awsec2_list: "${sp.name}"=>sp }
+     ami=data.aws_ami.amz_linux2.id
+     instance_type       = each.value.instance_type
+     vpc_security_group_ids = [aws_security_group.vpc-ssh.id,aws_security_group.vpc-web.id]
+      tags = {
+         name=each.value.name
+     }
 }
 
