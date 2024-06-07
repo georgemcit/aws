766f2bbce73024bcbe6661bd9845526e06f32e61
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-07 19:21:52 -0400

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,55 +1,17 @@
-/*
-#Create Security Group -SSH Traffic
-resource "aws_security_group" "vpc-ssh" {
-    name="vpc-ssh"
-    description = "Dev VPC SSH"
-    ingress{
-        description="Allow port 22"
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks=["0.0.0.0/0"]
-    }
-    egress{
-        description = "Allow all ip and ports outbound"
-        from_port=0
-        to_port=0
-        protocol="-1"
-        cidr_blocks=["0.0.0.0/0"]
-    }
-    tags = {
-      Name="vpc-ssh"
-    }
-}
+# Security Group for Public Bastion Host
+module "public_bastion_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  #version = "3.18.0"
+  #version = "4.0.0"
+  version = "5.1.0"  
 
-#Create Security Group - HTTP Traffic
-resource "aws_security_group" "vpc-web" {
-    name="vpc-web"
-    description = "Dev VPC web"
-    ingress{
-        description="Allow port 80"
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks=["0.0.0.0/0"]
-    }
-    ingress{
-        description="Allow port 443"
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-        cidr_blocks=["0.0.0.0/0"]
-    }
-    egress{
-        description = "Allow all ip and ports outbound"
-        from_port=0
-        to_port=0
-        protocol="-1"
-        cidr_blocks=["0.0.0.0/0"]
-    }
-    tags = {
-      Name="vpc-web"
-    }
+  name = "public-bastion-sg"
+  description = "Security Group with SSH port open for everybody (IPv4 CIDR), egress ports are all world open"
+  vpc_id = module.vpc.vpc_id
+  # Ingress Rules & CIDR Blocks
+  ingress_rules = ["ssh-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  # Egress Rule - all-all open
+  egress_rules = ["all-all"]
+  tags = local.common_tags
 }
-*/
-
