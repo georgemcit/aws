3f04ca0c47d7b443a7278601092f6167881849c6
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-07 18:58:59 -0400

    Update vairable.tf

--- vairable.tf
+++ vairable.tf
@@ -8,16 +8,21 @@ variable "aws_region" {
     description = "Region in which AWS Resources to be created"
     type = string
     default ="us-east-1"
-  
-}
-
-variable "instance_type"{
-    description="EC2 Instance type"
-    type =string
-    default="t3.micro"
 }
 variable "environment" {
   description = "Environment Variable used as a prefix"
   type = string
   default = "dev"
+}
+variable "instance_type" {
+  description = "EC2 Instance Type"
+  type = string
+  default = "t3.micro"  
+}
+
+# AWS EC2 Instance Key Pair
+variable "instance_keypair" {
+  description = "AWS EC2 Key pair that need to be associated with EC2 Instance"
+  type = string
+  default = "terraform-key"
 }
