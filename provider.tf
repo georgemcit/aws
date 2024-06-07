682c6ee641472bd8ad230768f529f67f3ca01a9f
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-07 19:57:08 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -19,3 +19,8 @@ variable "AWS_ACCESS_KEY"{
 variable "AWS_SECRET_ACCESS_KEY"{
   
 }
+}
+variable "aws_region" {
+    description = "Region in which AWS Resources to be created"
+    type = string
+    default ="us-east-1"
