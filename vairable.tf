dee87e77f8c6205d783e8b0c251d9dc9c39c6fca
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-07 19:36:58 -0400

    Update vairable.tf

--- vairable.tf
+++ vairable.tf
@@ -26,3 +26,20 @@ variable "instance_keypair" {
   type = string
   default = "terraform-key"
 }
+variable "aws_region" {
+  description = "Region in which AWS Resources to be created"
+  type = string
+  default = "us-east-1"  
+}
+# Environment Variable
+variable "environment" {
+  description = "Environment Variable used as a prefix"
+  type = string
+  default = "dev"
+}
+# Business Division
+variable "business_divsion" {
+  description = "Business Division in the large organization this Infrastructure belongs"
+  type = string
+  default = "sap"
+}
