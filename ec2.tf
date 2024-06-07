65851c1ebef16d25b03643dcbb4d94f98b953314
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-07 19:16:38 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,30 +1,17 @@
-/*
-resource "aws_instance""myec2m"{
-    ami=data.aws_ami.amz_linux2.id
-    instance_type = var.instance_type
-    vpc_security_group_ids = [aws_security_group.vpc-ssh.id,aws_security_group.vpc-web.id]
-    count = 2
-    tags = {
-        name="count-demo-${count.index}"
-    }
+# Bastion Host - EC2 Instance that will be created in VPC Public Subnet
+module "ec2_public" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  #version = "2.17.0"
+  version = "5.6.0"  
+  # insert the 10 required variables here
+  name                   = "${var.environment}-BastionHost"
+  #instance_count         = 5
+  ami                    = data.aws_ami.amzlinux2.id
+  instance_type          = var.instance_type
+  key_name               = var.instance_keypair
+  #monitoring             = true
+  subnet_id              = module.vpc.public_subnets[0]
+  #vpc_security_group_ids = [module.public_bastion_sg.this_security_group_id]
+  vpc_security_group_ids = [module.public_bastion_sg.security_group_id]
+  tags = local.common_tags
 }
-
-locals{
-  awsec2 = yamldecode(file("${path.module}/george/ec2.yaml"))
-  awsec2_list = [
-    for value in local.awsec2.listofec2: {
-        name=value.tagname
-        instance_type=value.instancetype
-      }
-    ]
-}
-resource "aws_instance""awsec2_george"{
-    for_each = {for instance in local.awsec2_list: "${instance.name}"=>instance}
-     ami=data.aws_ami.amz_linux2.id
-     instance_type       = each.value.instance_type
-     vpc_security_group_ids = [aws_security_group.vpc-ssh.id,aws_security_group.vpc-web.id]
-      tags = {
-         name=each.value.name
-     }
-}
-*/
