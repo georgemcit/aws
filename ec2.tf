697064edbe07a61b27d17f6f4300f68e922c8437
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-06 22:42:45 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,12 @@
+resource "aws_instance""myec2m"{
+    ami=data.aws_ami.amz_linux2.id
+    instance_type = var.instance_type
+    vpc_security_group_ids = [aws_security_group.vpc-ssh.id,aws_security_group.vpc-web.id]
+    count = 2
+    tags = {
+        name="count-demo-${count.index}"
+    }
+}
 
 locals{
 aws_ec2=yamldecode(file("${path.module}/$aws/ec2.yaml"))
@@ -8,7 +17,7 @@ aws_ec2=yamldecode(file("${path.module}/$aws/ec2.yaml"))
        }
     ]
 }
-resource "aws_instance""awsec2_george"{
+resource "aws_instance""myec2m"{
   for_each = {for sp in local.awsec2_list: "${sp.name}"=>sp }
   ami=data.aws_ami.amz_linux2.id
   instance_type       = each.value.instance_type
