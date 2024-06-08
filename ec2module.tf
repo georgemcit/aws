3dcc7dc7388e3dfae00ac517e22baeb345d4419d
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-07 20:31:28 -0400

    Update ec2module.tf

--- ec2module.tf
+++ ec2module.tf
@@ -1,10 +1,10 @@
 module "module_vnet" {
-  source               = "./aws/"
-  ami           = "data.aws_ami.amzn-linux-2023-ami.id"
-  instance_type = "c6a.2xlarge"
-  subnet_id     = "aws_subnet.george.id"
-  cidr_block_vpc    = ["172.16.0.0/16"]
-  vpc_id        = "aws_vpc.george.id"
+  source           = "./aws/"
+  ami              = "data.aws_ami.amzn-linux-2023-ami.id"
+  instance_type    = "c6a.2xlarge"
+  subnet_id        = "aws_subnet.george.id"
+  cidr_block_vpc   = ["172.16.0.0/16"]
+  vpc_id           = "aws_vpc.george.id"
   cidr_block_sb    =["172.16.10.0/24"]
-  tags          = "tf-george"
+  tags             = "tf-george"
 }
