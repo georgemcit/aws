38398b6f9bdc626ababa28d91ea0ff41278e7980
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-07 21:17:18 -0400

    Update ec2module.tf

--- ec2module.tf
+++ ec2module.tf
@@ -3,9 +3,9 @@ module "module_ec2t" {
   ami               = "data.aws_ami.amzn-linux-2023-ami.id"
   instance_type     = "c6a.2xlarge"
   subnet_id         = "aws_subnet.george.id"
-  vpc_cidr_block    = ["172.16.0.0/16"]
+  vpc_cidr_block    = [ ["172.16.0.0/16"]]
   vpc_id            = "aws_vpc.george.id"
-  sb_cidr_block     = ["172.16.10.0/24"]
+  sb_cidr_block     = [ ["172.16.10.0/24"]]
   tags              = "tf-george"
   availability_zone = "us-east-2a"
 }
