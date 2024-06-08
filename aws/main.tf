33481b4de0a31fbf8167e3fd808ee975cf6db0c1
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-07 20:05:34 -0400

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -23,4 +23,20 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
+resource "aws_vpc" "george_vpc" {
+  cidr_block = "172.16.0.0/16"
 
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_subnet" "george_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
