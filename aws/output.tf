ae19e2f2b9abf14e7ecfec805916b67e37ed330b
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-06-07 20:40:20 -0400

    Create output.tf

---
+++ aws/output.tf
@@ -0,0 +1,24 @@
+variable "ami"{
+  type=string
+}
+variable "instance_type"{
+  type=string
+}
+variable "subnet_id"{
+  type=string
+}
+variable "cidr_block_vpc"{
+  type=list(string)
+}
+variable "cidr_block_sb "{
+  type=list(string)
+}
+variable "vpc_id"{
+  type=string
+} 
+variable "atags"{
+  type=string
+}
+variable "availability_zone"{
+  type=string
+}
